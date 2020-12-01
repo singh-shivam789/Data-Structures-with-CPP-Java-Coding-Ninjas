a91e03fe42cb92735e0b242ba2ecb3bb1bd3477d
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2020-12-01 15:51:03 +0530

    Create Sum of Array.cpp

---
+++ Recursion 1/Sum of Array.cpp
@@ -0,0 +1,43 @@
+// Sum of Array
+// Given an array of length N, you need to find and return the sum of all elements of the array.
+
+// Input Format :
+// Line 1 : An Integer N i.e. size of array
+// Line 2 : N integers which are elements of the array, separated by spaces
+
+// Output Format :
+// Sum
+
+// Constraints :
+// 1 <= N <= 10^3
+
+// Sample Input 1 :
+// 3
+// 9 8 9
+
+// Sample Output 1 :
+// 26
+
+// Sample Input 2 :
+// 3
+// 4 2 1
+
+// Sample Output 2 :
+// 7    
+#include<iostream>
+using namespace std;
+int sum(int input[], int n) {
+	if(n == 1) return input[0];
+	return input[0] + sum(input+1, n-1);
+}
+int main(){
+    int n;
+    cin>>n;
+    int *input = new int[n];
+    for(int i=0; i<n; i++){
+        cin>>input[i];
+    }
+    cout<<sum(input, n);
+    delete [] input;
+    return 0;
+}
