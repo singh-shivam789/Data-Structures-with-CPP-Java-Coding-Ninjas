3e9d80ae89fa8517ccad817845c65c2a9d102ecd
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2020-12-02 22:30:17 +0530

    Create Count Zeros.cpp

---
+++ Recursion 1a/Count Zeros.cpp
@@ -0,0 +1,42 @@
+// Count Zeros
+// Given an integer N, count and return the number of zeros that are present in the given integer using recursion.
+
+// Input Format :
+// Integer N
+
+// Output Format :
+// Number of zeros in N
+
+// Constraints :
+// 0 <= N <= 10^9
+
+// Sample Input 1 :
+// 10204
+
+// Sample Output 1 :
+// 2
+
+// Sample Input 2 :
+// 708000
+
+// Sample Output 2 :
+// 4
+
+#include<iostream>
+using namespace std;
+int countZeros(int n) {
+    if(n<=9){
+        if(n == 0) return 1;
+        else return 0;
+    }
+    int ans = countZeros(n/10);
+    if(n%10 == 0) return ans + 1;
+    else return ans;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<countZeros(n)<<endl;
+	return 0;
+}
+
