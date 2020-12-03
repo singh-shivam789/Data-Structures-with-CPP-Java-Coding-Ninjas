76769fde558bd2b289bf63d3bc426194c6ab1394
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2020-12-03 07:19:19 +0530

    Create Check Palindrome.cpp

---
+++ Recursion 1a/Check Palindrome.cpp
@@ -0,0 +1,48 @@
+// Check Palindrome (recursive)
+// Check whether a given String S is a palindrome using recursion. Return true or false.
+
+// Input Format :
+// String S
+
+// Output Format :
+// 'true' or 'false'
+
+// Constraints :
+// 0 <= |S| <= 1000
+// where |S| represents length of string S.
+
+// Sample Input 1 :
+// racecar
+
+// Sample Output 1:
+// true
+
+// Sample Input 2 :
+// ninja
+
+// Sample Output 2:
+// false
+
+#include<bits/stdc++.h>
+using namespace std;
+bool checkPalindrome(char input[]) {
+    // Write your code here
+    if(strlen(input) == 0) return true;
+    if(input[0] == input[strlen(input) - 1]){
+        input[strlen(input) - 1] = '\0';
+        return checkPalindrome(input + 1);
+    }
+    else return false;
+}
+
+int main(){
+    char input[50];
+    cin >> input;
+
+    if(checkPalindrome(input)) {
+        cout << "true" << endl;
+    }
+    else {
+        cout << "false" << endl;
+    }
+}
