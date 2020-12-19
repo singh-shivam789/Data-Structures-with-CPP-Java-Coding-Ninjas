0ba4aebd6236361e869034188bd47999ec66adfb
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2020-12-19 18:34:50 +0530

    Create Quick Sort.cpp

---
+++ Recursion 2/Quick Sort.cpp
@@ -0,0 +1,71 @@
+// Quick Sort Code
+// Sort an array A using Quick Sort.
+// Change in the input array itself. So no need to return or print anything.
+
+// Input format :
+// Line 1 : Integer n i.e. Array size
+// Line 2 : Array elements (separated by space)
+
+// Output format :
+// Array elements in increasing order (separated by space)
+
+// Constraints :
+// 1 <= n <= 10^3
+
+// Sample Input 1 :
+// 6 
+// 2 6 8 5 4 3
+
+// Sample Output 1 :
+// 2 3 4 5 6 8
+
+// Sample Input 2 :
+// 5
+// 1 5 2 7 3
+
+// Sample Output 2 :
+// 1 2 3 5 7 
+
+#include<bits/stdc++.h>
+using namespace std;
+int partition(int input[], int s, int e){
+    int i = s - 1;
+    int j = s;
+    int pivot = e;
+    for(; j<e;){
+        if(input[j] < input[pivot]){
+            ++i;
+            swap(input[i], input[j]);
+            ++j;
+        }
+        else ++j;
+    }
+    swap(input[i+1], input[pivot]);
+    return i+1;
+}
+void quickSortHelper(int input[], int s, int e){
+    if(s<e){
+        int p = partition(input, s, e);
+        quickSortHelper(input, s, p-1);
+        quickSortHelper(input, p+1, e);
+    }
+}
+void quickSort(int input[], int size) {
+    return quickSortHelper(input, 0, size-1);
+}
+int main(){
+    int n;
+    cin >> n;
+    int *input = new int[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+
+    quickSort(input, n);
+
+    for(int i = 0; i < n; i++) {
+        cout << input[i] << " ";
+    }
+    delete [] input;
+}
