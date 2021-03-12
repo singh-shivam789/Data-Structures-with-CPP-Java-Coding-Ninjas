5f09b023e291efceb450898503990a6331f74511
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2021-03-12 18:56:02 +0530

    Update Pair Star.cpp

--- Recursion 1b/Pair Star.cpp
+++ Recursion 1b/Pair Star.cpp
@@ -27,14 +27,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 void pairStar(char input[]) {
-	if(strlen(input) == 0) return;
-	if(input[0] == input[1]){
-        for(int i=strlen(input); i>=1; i--) input[i+1] = input[i];
+	if(strlen(input) == 1) return;
+    if(input[0] == input[1]){
+        for(int i=strlen(input); i>=1; i--){
+            input[i+1] = input[i];
+        }
         input[1] = '*';
-        return pairStar(input + 2);
+        pairStar(input + 2);
     }
-    else return pairStar(input + 1);
+    else{
+        pairStar(input + 1);
+    }
 }
+
 int main(){
      char input[100];
    cin.getline(input, 100);
