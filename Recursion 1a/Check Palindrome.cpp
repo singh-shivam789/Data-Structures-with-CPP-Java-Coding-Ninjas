a095063d1dd1c2dd368d466694d434f8985ec5c4
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2021-03-12 17:28:54 +0530

    Update Check Palindrome.cpp

--- Recursion 1a/Check Palindrome.cpp
+++ Recursion 1a/Check Palindrome.cpp
@@ -26,11 +26,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 bool checkPalindrome(char input[]) {
-    // Write your code here
-    if(strlen(input) == 0) return true;
-    if(input[0] == input[strlen(input) - 1]){
+	if(strlen(input) == 1 || strlen(input) == 0) return true;
+	if(input[0] == input[strlen(input) - 1]){
         input[strlen(input) - 1] = '\0';
-        return checkPalindrome(input + 1);
+        return checkPalindrome(input+1);
     }
     else return false;
 }
