7eb0bc9b503b20866970d7b73a79d02ef33e82cc
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2021-03-12 17:16:40 +0530

    Update Count Zeros.cpp

--- Recursion 1a/Count Zeros.cpp
+++ Recursion 1a/Count Zeros.cpp
@@ -25,12 +25,12 @@
 #include<iostream>
 using namespace std;
 int countZeros(int n) {
-    if(n<=9){
-        if(n == 0) return 1;
-        else return 0;
-    }
+	if(n/10 == 0 && n%10 == 0) return 1;
+    else if(n/10 == 0) return 0;
     int ans = countZeros(n/10);
-    if(n%10 == 0) return ans + 1;
+    if(n%10 == 0){
+        return ans + 1;
+    }
     else return ans;
 }
 int main(){
