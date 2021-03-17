da7ee747e99c1af289bdedcb620a721755d4b452
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2021-03-17 13:07:55 +0530

    Update Quick Sort.cpp

--- Recursion 2/Quick Sort.cpp
+++ Recursion 2/Quick Sort.cpp
@@ -28,31 +28,47 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+
 int partition(int input[], int s, int e){
-    int i = s - 1;
-    int j = s;
-    int pivot = e;
-    for(; j<e;){
-        if(input[j] < input[pivot]){
-            ++i;
-            swap(input[i], input[j]);
-            ++j;
+    int pivot = input[s], count = 0;
+    for(int i=s+1; i<=e; i++){
+        if(input[i] <= pivot){
+            count++;
         }
-        else ++j;
     }
-    swap(input[i+1], input[pivot]);
-    return i+1;
+    int pivotIndex = s + count;
+    swap(input[s], input[pivotIndex]);
+
+    //partitioning
+
+    int i=s, j=e;
+    while(i<pivotIndex && j>pivotIndex){
+        if(input[i] <= pivot){
+            i++; 
+        }
+        else if(input[j] > pivot){
+            j--; 
+        }
+        else{
+            swap(input[i], input[j]);
+            i++, j--;
+        }
+    }
+    return pivotIndex;
 }
 void quickSortHelper(int input[], int s, int e){
-    if(s<e){
-        int p = partition(input, s, e);
-        quickSortHelper(input, s, p-1);
-        quickSortHelper(input, p+1, e);
+    if(s >= e) return;
+    else{
+        int pivot = partition(input, s, e);
+        quickSortHelper(input, s, pivot-1);
+        quickSortHelper(input, pivot+1, e);
     }
 }
 void quickSort(int input[], int size) {
-    return quickSortHelper(input, 0, size-1);
+
+    quickSortHelper(input,  0, size-1);
 }
+
 int main(){
     int n;
     cin >> n;
