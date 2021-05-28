328989a16356ca696efd5258ff88a8de10def1b7
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2021-05-28 14:19:50 +0530

    Create Length of LL.cpp

---
+++ Linked List 1/Length of LL.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node*next;
+    
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+Node* takeInput(){
+    int data;
+    cin>>data;
+    Node* head = NULL;
+    Node* tail = NULL;
+    while(data!=-1){
+        Node* newNode = new Node(data);
+        if(head == NULL){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail -> next = newNode;
+            tail = newNode;
+        }
+        cin>>data;
+    }
+    return head;
+}
+int length(Node* head){
+  Node* temp = head;
+  int l = 0;
+  while(temp != NULL){
+    ++l;
+    temp = temp->next;
+  }
+  return l;
+}
+int main() {
+  Node*head = takeInput();
+	int l = length(head);
+  cout<<l<<endl;
+  return 0;
+}
