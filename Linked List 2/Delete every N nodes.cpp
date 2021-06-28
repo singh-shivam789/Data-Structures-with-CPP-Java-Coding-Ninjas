c98d3845026b80bba34e18694b10fd0b084d7b8d
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2021-06-28 21:02:31 +0530

    Update Delete every N nodes.cpp

--- Linked List 2/Delete every N nodes.cpp
+++ Linked List 2/Delete every N nodes.cpp
@@ -1,7 +1,9 @@
 /*
 Delete every N nodes
 
-You have been given a singly linked list of integers along with two integers, 'M,' and 'N.' Traverse the linked list such that you retain the 'M' nodes, then delete the next 'N' nodes. Continue the same until the end of the linked list.
+You have been given a singly linked list of integers along with two integers, 'M,' and 'N.' Traverse the linked list such that you retain the 'M' nodes, 
+then delete the next 'N' nodes. Continue the same until the end of the linked list.
+
 To put it in other words, in the given linked list, you need to delete N nodes after every M nodes.
 
 Input format :
