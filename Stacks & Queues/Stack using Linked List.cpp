20b59e5144596fe725d3e846eba1f3a64f06fc71
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2021-07-28 20:44:53 +0530

    Update comments

--- Stacks & Queues/Stack using Linked List.cpp
+++ Stacks & Queues/Stack using Linked List.cpp
@@ -23,52 +23,25 @@ It returns the size of the stack at any given instance of time.
 6. isEmpty() :
 It returns a boolean value indicating whether the stack is empty or not.
 
-Output for every query will be printed in a separate line.
-
-Note:
-You are not required to print anything explicitly. It has already been taken care of. Just implement the function.
-
-Constraints:
-1 <= q <= 10^5
-1 <= x <= 5
--2^31 <= data <= 2^31 - 1 and data != -1
-Where 'q' is the total number of queries being performed on the stack, 'x' is the range for every query and data represents the integer pushed into the stack. 
+Output for every query should be printed in a separate line.
 
 Time Limit: 1 second
 
-Sample Input 1:
-6
-1 13
-1 47
-4
-5
-2
-3
+Sample Input:
+100
+101
+102
+103
+104
 
-Sample Output 1:
-2
-false
-47
-13
+Sample Output:
+ h
+ h
+ g
+ f
+ 2
+ 0
 
-Sample Input 2:
-4
-5
-2
-1 10
-5
-
-Sample Output 2:
-true
--1
-false 
-
-Explanation of Sample Input 2:
-There are 4 queries in total.
-The first one is Query-5: It tells whether the stack is empty or not. Since the stack is empty at this point, the output is  'true'.
-The second one is Query-2: It pops the data from the stack. Since at this point in time, no data exist in the stack hence, it prints -1.
-The third one is Query-1: It pushes the specified data 10 into the stack and since the function doesn't return anything, nothing is printed.
-The fourth one is Query-5: It tells whether the stack is empty at this point or not. Since the stack has one element and hence it is not empty, false is printed.
 */
 
 #include<iostream>
@@ -158,14 +131,4 @@ int main() {
 
     cout << s.getSize() << endl;
     cout << s.isEmpty() << endl;
-    
-    /*
-        Output: 
-        h
-        h
-        g
-        f
-        2
-        0
-    */
 }
