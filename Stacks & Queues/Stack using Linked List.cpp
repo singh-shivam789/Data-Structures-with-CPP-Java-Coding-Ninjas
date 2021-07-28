0ec02c969c8d35a0482a55fe4827fe82c5f8da9a
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2021-07-28 20:34:00 +0530

    Update Stack using Linked List.cpp

--- Stacks & Queues/Stack using Linked List.cpp
+++ Stacks & Queues/Stack using Linked List.cpp
@@ -9,40 +9,19 @@ You need to implement the following public functions :
 It initialises the data members as required.
 
 2. push(data) :
-This function should take one argument of type integer. It pushes the element into the stack and returns nothing.
+This function should take one argument of any data-type. It pushes the element into the stack and returns nothing.
 
 3. pop() :
-It pops the element from the top of the stack and in turn, returns the element being popped or deleted. In case the stack is empty, it returns -1.
+It pops the element from the top of the stack and in turn, returns the element being popped or deleted. In case the stack is empty, it returns 0.
 
 4. top :
-It returns the element being kept at the top of the stack. In case the stack is empty, it returns -1.
+It returns the element being kept at the top of the stack. In case the stack is empty, it returns 0.
 
 5. size() :
 It returns the size of the stack at any given instance of time.
 
 6. isEmpty() :
 It returns a boolean value indicating whether the stack is empty or not.
-
-Operations Performed on the Stack:
-Query-1(Denoted by an integer 1): Pushes an integer data to the stack.
-Query-2(Denoted by an integer 2): Pops the data kept at the top of the stack and returns it to the caller.
-Query-3(Denoted by an integer 3): Fetches and returns the data being kept at the top of the stack but doesn't remove it, unlike the pop function.
-Query-4(Denoted by an integer 4): Returns the current size of the stack.
-Query-5(Denoted by an integer 5): Returns a boolean value denoting whether the stack is empty or not.
-
-Input Format:
-The first line contains an integer 'q' which denotes the number of queries to be run against each operation in the stack. 
-Then the test cases follow.
-Every 'q' lines represent an operation that needs to be performed.
-For the push operation, the input line will contain two integers separated by a single space, representing the type of the operation in integer and the integer data being pushed into the stack.
-For the rest of the operations on the stack, the input line will contain only one integer value, representing the query being performed on the stack.
-
-Output Format:
-For Query-1, you do not need to return anything.
-For Query-2, prints the data being popped from the stack.
-For Query-3, prints the data kept on the top of the stack.
-For Query-4, prints the current size of the stack.
-For Query-5, prints 'true' or 'false'(without quotes).
 
 Output for every query will be printed in a separate line.
 
@@ -57,7 +36,7 @@ Where 'q' is the total number of queries being performed on the stack, 'x' is th
 
 Time Limit: 1 second
 
-ample Input 1:
+Sample Input 1:
 6
 1 13
 1 47
