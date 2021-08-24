6238798adbe807731d1a579aedc9f24cadbac72f
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2021-08-24 21:03:17 +0530

    Create Delete Tree function.cpp

---
+++ Trees/Delete Tree function.cpp
@@ -0,0 +1,85 @@
+/*
+Function to delete a tree recursively
+*/
+
+#include<climits>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <climits>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+void printLevelWise(TreeNode<int>* root) {
+    queue<TreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << front->data << ":";
+        for (int i = 0; i < front->children.size(); i++) {
+            TreeNode<int>* currentNode = front->children[i];
+            pendingNodes.push(currentNode);
+            if (i + 1 == front->children.size()) {
+                cout << currentNode->data;
+            }
+            else {
+                cout << currentNode->data << ",";
+            }
+        }
+        cout << "\n";
+    }
+}
+
+void deleteTree(TreeNode<int>* root) {
+    if (root == NULL) return;
+    for (int i = 0; i < root->children.size(); i++) {
+        deleteTree(root->children[i]);
+    }
+    delete root;
+}
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    printLevelWise(root);
+    deleteTree(root);
+}
