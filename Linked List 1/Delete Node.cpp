49327c1d3fe1828322dafcd734a487ccff4fbdd9
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2022-01-26 12:31:05 +0530

    Update Delete Node.cpp

--- Linked List 1/Delete Node.cpp
+++ Linked List 1/Delete Node.cpp
@@ -50,14 +50,13 @@ Sample Output 2 :
 using namespace std;
 
 class Node{
-public:
-	int data;
-	Node *next;
-	Node(int data)
-	{
-		this->data = data;
-		this->next = NULL;
-	}
+    public:
+    int data;
+    Node *next;
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
 };
 
 int length(Node* head){
@@ -69,71 +68,70 @@ int length(Node* head){
     return l;
 }
 
-Node *deleteNode(Node *head, int n){
-       if(length(head) <= n){
-       return head;
-    }   
-    else if(n == 0){
-        Node* nextNode = head->next;
-        Node* temp = head;
-        head = nextNode;
-        delete temp;
+Node *deleteNode(Node *head, int i){
+    if(head == NULL){
         return head;
+    }
+    else if(i == 0){
+        return head -> next;
     }
     else{
         int count = 0;
         Node* temp = head;
-        while(count < n-1){
+        while(temp != NULL && count < i-1){
+            temp = temp -> next;
             count++;
-            temp = temp->next;
         }
-        Node* a = temp->next;
-        Node* b = a->next;
-        temp->next = b;
-        delete a;
+        if(temp != NULL && temp -> next != NULL){
+            Node* target = temp -> next;
+            temp -> next = target -> next;
+            target -> next = NULL;
+            delete target;
+        }
+
         return head;
     }
 }
 
 Node *takeinput(){
-	int data;
-	cin >> data;
-	Node *head = NULL, *tail = NULL;
-	while (data != -1){
-		Node *newNode = new Node(data);
-		if (head == NULL){
-			head = newNode;
-			tail = newNode;
-		}
-		else{
-			tail->next = newNode;
-			tail = newNode;
-		}
-		cin >> data;
-	}
-	return head;
+    int data;
+    cin >> data;
+    Node *head = NULL, *tail = NULL;
+    while (data != -1){
+        Node *newNode = new Node(data);
+        if (head == NULL){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
 }
 
 void print(Node *head){
-	Node *temp = head;
-	while (temp != NULL){
-		cout << temp->data << " ";
-		temp = temp->next;
-	}
-	cout << endl;
+    Node *temp = head;
+    while (temp != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
 }
 
 int main(){
-	int t;
-	cin >> t;
+    int t;
+    cin >> t;
 
-	while (t--){
-		Node *head = takeinput();
-		int pos;
-		cin >> pos;
-		head = deleteNode(head, pos);
-		print(head);
-	}
+    while (t--){
+        Node *head = takeinput();
+        int pos;
+        cin >> pos;
+        head = deleteNode(head, pos);
+        print(head);
+    }
 
-	return 0;
+    return 0;
 }
