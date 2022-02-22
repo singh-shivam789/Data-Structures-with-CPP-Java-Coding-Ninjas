ad9d06dda900aa3af4c6d8bedf35916bf9123b84
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2022-02-22 18:09:09 +0530

    Create DFS Traversal.cpp

---
+++ Graphs 1/DFS Traversal.cpp
@@ -0,0 +1,103 @@
+/*
+DFS Traversal
+Given an undirected and disconnected graph G(V, E), print its DFS traversal.
+
+Note:
+1. Here you need to consider that you need to print DFS path starting from vertex 0 only. 
+2. V is the number of vertices present in graph G and vertices are numbered from 0 to V-1. 
+3. E is the number of edges present in graph G.
+4. Take graph input in the adjacency matrix.
+5. Handle for Disconnected Graphs as well
+
+Input Format :
+The first line of input contains two integers, that denote the value of V and E.
+Each of the following E lines contains space separated two integers, that denote that there exists an edge between vertex a and b.
+
+Output Format :
+Print the DFS Traversal, as described in the task.
+
+Constraints :
+0 <= V <= 1000
+0 <= E <= (V * (V - 1)) / 2
+0 <= a <= V - 1
+0 <= b <= V - 1
+
+Time Limit: 1 second
+
+Sample Input 1:
+4 4
+0 1
+0 3
+1 2
+2 3
+
+Sample Output 1:
+0 1 3 2
+*/
+
+#include <iostream>
+using namespace std;
+
+void printDFS(int v, int ** edges, bool * visited, int sv){
+    cout << sv <<" ";
+    visited[sv] = true;
+    for(int i=0; i<v; i++){
+        if(sv == i) continue;
+        if(edges[sv][i] == 1 && !visited[i]){
+            printDFS(v, edges, visited, i);
+        }
+    }
+}
+
+void DFS(int v, int ** edges){
+    bool * visited = new bool[v];
+    for (int i = 0; i < v; i++)
+    {
+        visited[i] = false;
+    }
+    
+    for(int i=0; i<v; i++){
+    	if(!visited[i]){
+            printDFS(v, edges, visited, i);
+        }    
+    }
+    
+    delete [] visited;
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    if(n == 0)
+        return 0;
+    else if(e == 0){
+        for(int i=0; i<n; i++){
+            cout<<i<<" ";            
+        }
+        return 0;
+    }
+
+    int **edges = new int*[n];
+
+
+    for (int i = 0; i < n; i++)
+    {
+        edges[i] = new int[n];
+        for (int j = 0; j < n; j++)
+        {
+            edges[i][j] = 0;
+        }
+    }
+
+    int f, s;
+    for (int i = 0; i < e; i++)
+    {
+        cin >> f >> s;
+        edges[f][s] = 1;
+        edges[s][f] = 1;
+    }
+
+    DFS(n, edges);
+}
