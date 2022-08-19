1b7cb7b572cc9a50e3bec410acbfe5858d6b8c45
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2022-08-19 17:46:31 +0530

    Update Sort K Sorted Array.java

--- Priority Queue/Sort K Sorted Array.java
+++ Priority Queue/Sort K Sorted Array.java
@@ -1,3 +1,22 @@
+/*
+Sort K Sorted Array
+
+Given an array of n elements, where each element is at most k away from its target position, devise an algorithm that sorts in O(n log k) time. 
+For example, let us consider k is 2, an element at index 7 in the sorted array, can be at indexes 5, 6, 7, 8, 9 in the given array.
+
+Examples: 
+Input : arr[] = {6, 5, 3, 2, 8, 10, 9} k = 3 
+Output : arr[] = {2, 3, 5, 6, 8, 9, 10}
+
+Input : arr[] = {10, 9, 8, 7, 4, 70, 60, 50} k = 4
+Output : arr[] = {4, 7, 8, 9, 10, 50, 60, 70}
+
+Time complexity should be O(n * logk) and space complexity should not be more than O(k).
+
+Note: Order of elements in the output is not important.
+
+*/
+
 import java.util.*;
 public class Main{
     
